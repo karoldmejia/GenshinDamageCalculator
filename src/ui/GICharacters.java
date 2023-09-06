fdbbecaca6010b7879255e7327ddfe66a38cbf51
unknown <karold mejia>
2023-09-05 23:21:38 -0500

    new classes

---
+++ src/ui/GICharacters.java
@@ -0,0 +1,16 @@
+package ui;
+import java.util.*;
+
+public class GICharacters {
+    double atk, critr, crit, elm;
+    String nameChar;
+
+    GICharacters(double atk, double critr, double crit, double elm, String nameChar){
+        this.atk=atk;
+        this.critr=0.05;
+        this.crit=0.5;
+        this.nameChar=nameChar;
+    }
+
+
+}
